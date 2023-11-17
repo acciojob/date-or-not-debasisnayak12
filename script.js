91ff1060b486145d541cb1b26ce7c30bc78e7f9a
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-17 12:18:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var isDate = function (input) {
   //   write your code here
 	if(typeof input === 'string'){
-		return Date.parse(input) !== NaN;
-	}else if(input instanceof Date){
+		return !isNaN(Date.parse(input));
+	}else if(input instanceof Date){ 
 		return !isNaN(input.getTime());
-	}else{ 
-		return false; 
+	}else{  
+		return false;  
 	}
 };
 
