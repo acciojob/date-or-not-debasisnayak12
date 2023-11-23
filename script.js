e5c02be8068926216e533252df44fbfb27bf1391
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-23 10:45:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var isDate = function (input) {
   //   write your code here
-	if(typeof input === 'string'){
-		return !isNaN(Date.parse(input));
-	}else if(input instanceof Date){ 
-		return !isNaN(input.getTime());
-	}else{  
-		return false;  
-	}
+	// if(typeof input === 'string'){
+	// 	return !isNaN(new Date(input).getTime());
+	// }else{
+	// 	return input instanceof Date && !isNaN(input.getTime());
+	// }
+
+	return !isNaN(new Date(input));
 };
 
-// Do not change the code below.
-const input = prompt("Enter Date.");  
+// Do not change the code below. 
+const input = prompt("Enter Date.");      
 alert(isDate(input));  
